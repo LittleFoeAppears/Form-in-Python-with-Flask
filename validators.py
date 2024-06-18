c2c4da897898edffd047709bc895d93b4e7ce21e
LittleFoeAppears <155742589+LittleFoeAppears@users.noreply.github.com>
2024-06-18 14:25:44 +0200

    Delete validators.py

--- validators.py
+++
@@ -1,5 +0,0 @@
-from wtforms import ValidationError
-
-def validate_message_length(form, field):
-    if len(field.data) < 10:
-        raise ValidationError('Message must be at least 10 characters long')
